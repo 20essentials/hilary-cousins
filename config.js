41a34da4914429f20bc7ce165f37de2faadda041
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:42:47 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HILARY COUSINS';
+export const nameSongs = [
+	"RICHEST MAN IN THE WORLD", /*1*/
+	"ISLAND", /*2*/
+	"TAKE ME HOME", /*3*/
+	"CANADA", /*4*/
+	"MEDITATION FROM THAIS", /*5*/
+	"CAC BLAC", /*6*/
+	"VUI SE PULISIA", /*7*/
+	"TU I TU", /*8*/
+	"VAN DA LLESTUS", /*9*/
+	"CHIKAGO", /*10*/
+	"LA CLIKA", /*11*/
+	"KE FARIA", /*12*/
+	"PENTHAUS", /*13*/
+	"AMIM PARAN PAL CARRE", /*14*/
+	"OU SHIT", /*15*/
+	"ANIMALS", /*16*/
+	"AN AL COLE", /*17*/
+	"TATIANA RAXETS", /*18*/
+	"IAA", /*19*/
+	"PORTU UNA", /*20*/
+]
